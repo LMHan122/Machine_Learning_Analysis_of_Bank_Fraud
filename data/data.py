4b736c18dd6a6a57dca8cd9fa2f9576fc78a66c7
Leslie Hanson <lesliemhanson@gmail.com>
2024-12-02 14:18:33 -0600

    Tested environment.yml file. Corrected the name of the file so it would run correctly. Also moved the location of the data.py file, and changed the destination_dir path name to match.

--- data/data.py
+++ data/data.py
@@ -5,7 +5,7 @@ import shutil
 path = kagglehub.dataset_download("priyamchoksi/credit-card-transactions-dataset")
 
 # Define the destination directory
-destination_dir = r"/data"
+destination_dir = r"C:\Users\eelil\OneDrive\Desktop\Capstone\Machine_Learning_Analysis_of_Bank_Fraud\data"
 
 # Move the files
 shutil.move(path, destination_dir)
