9026266f796834919d53e55caccbddccde410511
Leslie Hanson <lesliemhanson@gmail.com>
2024-12-10 18:07:31 -0600

    corrected some issues with cust_loc_map.py so the objects can be used in main.py. Also added docstrings to the functions.

--- data/data_preparation/cust_loc_map.py
+++ data/data_preparation/cust_loc_map.py
@@ -10,26 +10,16 @@ logging.basicConfig(
 )
 logger = logging.getLogger()
 
-# grabbing cleaned dataset from WandB
-logging.info('Getting the dataset from WandB')
-run = wandb.init(project="credit_card_fraud", save_code=True)
-artifact = run.use_artifact(
-    "lhan122-student/credit_card_fraud/cleaned_credit_card_data:latest", type="dataset"
-)
-artifact_dir = artifact.download()
-file_path = os.path.join(artifact_dir, "cleaned_credit_card_fraud.parquet")
-df = pd.read_parquet(file_path)
-
-
-# creating map for customer location
-cust_loc = df[["city", "state"]].drop_duplicates()
-
-# getting lat and long values for city and state
-geolocator = Nominatim(user_agent="geoapi")
-
-
-# creating function to be applied
 def get_lat_long(city, state):
+    '''
+    This function is used to get the latitude and longitude of a city
+    and state combination using the geopy API.
+    :param city: The city column in the df
+    :param state: The state column in the df
+    :return: Nothing - the vaules are appended onto the df
+    '''
+    geolocator = Nominatim(user_agent="geoapi")
+
     try:
         location = geolocator.geocode(f"{city}, {state}")
         if location:
@@ -43,23 +33,51 @@ def get_lat_long(city, state):
     finally:
         sleep(1)
 
-logger.info('Getting the geocoding file')
-# apply geocoding
-cust_loc["lat_long"] = cust_loc.apply(
-    lambda x: get_lat_long(x["city"], x["state"]), axis=1
-)
-cust_loc[["cust_lat", "cust_long"]] = pd.DataFrame(
-    cust_loc["lat_long"].tolist(), index=cust_loc.index
-)
+def cust_loc_map():
+    '''
+    This function is used to get the location of each city and state
+    using the get_lat_long function and use it to create two new columns
+    and populating the values.
+    :return: None
+    '''
+    # grabbing cleaned dataset from WandB
+    logger.info('Getting the dataset from WandB')
+    run = wandb.init(project="credit_card_fraud")
+    artifact = run.use_artifact(
+        "lhan122-student/credit_card_fraud/cleaned_credit_card_data:latest", type="dataset"
+    )
+    artifact_dir = artifact.download()
+    file_path = os.path.join(artifact_dir, "cleaned_credit_card_fraud.parquet")
+    df = pd.read_parquet(file_path)
 
-# uploading it to WandB as well for record keeping
-cust_loc_file = "cust_loc.parquet"
-cust_loc.to_parquet(cust_loc_file, index=False)
 
-artifact = wandb.Artifact(
-    name="cust_loc_data",
-    type="dataset",
-    description="Lat and Long values for customer's city and state.",
-)
-artifact.add_file(cust_loc_file)
-run.log_artifact(artifact)
+    # creating map for customer location
+    logger.info('Creating side df of unique city and state combinations')
+    cust_loc = df[["city", "state"]].drop_duplicates()
+
+
+    # getting lat and long values for city and state
+    logger.info('Getting the geocoding file')
+    cust_loc["lat_long"] = cust_loc.apply(
+        lambda x: get_lat_long(x["city"], x["state"]), axis=1
+    )
+    cust_loc[["cust_lat", "cust_long"]] = pd.DataFrame(
+        cust_loc["lat_long"].tolist(), index=cust_loc.index
+    )
+
+    # uploading it to WandB as well for record keeping
+    logger.info("Uploading file to WandB.")
+    cust_loc_file = "data/data_preparation/cust_loc.parquet"
+    cust_loc.to_parquet(cust_loc_file, index=False)
+
+    artifact = wandb.Artifact(
+        name="cust_loc_data",
+        type="dataset",
+        description="Lat and Long values for customer's city and state.",
+    )
+    artifact.add_file(cust_loc_file)
+    run.log_artifact(artifact)
+    run.finish()
+
+if __name__ == '__main__':
+    cust_loc_map()
--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-from data.data_preparation.cust_loc_map import get_lat_long
+from data.data_preparation.cust_loc_map import get_lat_long, cust_loc_map
 import os
+import logging
 
-
+#setting up logger
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)-20s %(message)s", filemode="a"
+)
+logger = logging.getLogger()
 
 
 if __name__ == '__main__':
 
 
-    if not os.path.exists('data/data_preparation/cust_loc.parquet'):
-        get_lat_long()
+
+
+    #if city and state location values don't already exist
+    #new file will be created getting these values
+    logger.info('Checking if latitude and longitude map exists')
+    cust_map_file_path = 'data/data_preparation/cust_loc.parquet'
+    if not os.path.exists(cust_map_file_path):
+        logger.info('Getting City and State latitude and longitude values.')
+        cust_loc_map()
