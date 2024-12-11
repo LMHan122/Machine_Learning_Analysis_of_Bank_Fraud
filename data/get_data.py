a5ec6382ab80d7fa2a23477cb33f7ddf4fadb97a
Leslie Hanson <lesliemhanson@gmail.com>
2024-12-10 18:33:08 -0600

    Made some minor changes to get_data.py and added the function to the main.py file

--- data/get_data.py
+++ data/get_data.py
@@ -28,45 +28,45 @@ def data_download():
         - Kaggle and Weights and Biases credentials will need to be added before calling this function.
     """
 
-    # Connecting to wandb
+    # connecting to wandb
     run = wandb.init(project="credit_card_fraud", job_type="data_download")
 
-    # Creating log message for start of download
+    # starting download
     logger.info("Downloading data from Kaggle")
-
-    # Downloading data
     path = kagglehub.dataset_download("priyamchoksi/credit-card-transactions-dataset")
 
-    # Creating destination
-    destination_dir = r""
+    # creating destination and moving the files
+    destination_dir = "data"
     os.makedirs(destination_dir, exist_ok=True)
-
-    # Move the files
     logger.info("Moving data to %s locally" % destination_dir)
     shutil.move(path, destination_dir)
 
-    # Locating the csv file within the downloaded folder
+    # locating the csv file within the downloaded folder
     downloaded_folder = os.path.join(
         destination_dir, "1"
-    )  # KaggleHub puts files in a folder labeled '1'
+    )  # KaggleHub puts csv in a folder labeled '1'
     csv_files = [f for f in os.listdir(downloaded_folder) if f.endswith(".csv")]
 
-    # Log an error if data is not there
+    # log an error if data is not there
     if not csv_files:
-        logger.error("No CSV files found in the downloaded data.")
+        logger.error("No CSV files found in the downloaded.")
         run.finish()
         return
     else:
         csv_file_path = os.path.join(downloaded_folder, csv_files[0])
 
-        # Uploading the csv file to wandb
+        # uploading the csv file to wandb
         logger.info("Uploading data to Weights & Biases")
-        artifact = wandb.Artifact(name="credit_card_data", type="dataset")
+        artifact = wandb.Artifact(
+            name="credit_card_data",
+            type="dataset",
+            description="Original data directly from Kaggle.",
+        )
         artifact.add_file(csv_file_path)
         run.log_artifact(artifact)
         logger.info("Data upload to Weights & Biases complete.")
 
-    # Finishing the run
+    # finishing the run
     run.finish()
 
 
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from data.data_preparation.cust_loc_map import get_lat_long, cust_loc_map
+from data.get_data import data_download
 import os
 import logging
 
@@ -10,11 +11,22 @@ logger = logging.getLogger()
 
 
 if __name__ == "__main__":
+    # getting data from Kaggle if file hasn't been downloaded already
+    logger.info("Checking if dataset exists")
+    orig_data_path = "data/1/credit_card_transactions.csv"
+    if not os.path.exists(orig_data_path):
+        logger.info("Downloading original dataset")
+        data_download()
+    else:
+        logger.info("Original dataset exists")
 
-    # if city and state location values don't already exist
+    # if city and state location map values don't already exist
     # new file will be created getting these values
     logger.info("Checking if latitude and longitude map exists")
     cust_map_file_path = "data/data_preparation/cust_loc.parquet"
+
     if not os.path.exists(cust_map_file_path):
         logger.info("Getting City and State latitude and longitude values.")
         cust_loc_map()
+    else:
+        logger.info("City and State latitude and longitude map exists")
